7c28d8e9354f0867b89cd8d47c2305bfdc93a10f
Esaú Gómez <alfonsoesaugomez@gmail.com>
2025-07-15 16:20:53 -0600

    test(integration): tests for quiz with skipRewards

--- core/api/test/integration/app/quizzes/claim-quiz.spec.ts
+++ core/api/test/integration/app/quizzes/claim-quiz.spec.ts
@@ -12,6 +12,8 @@ import * as RateLimitImpl from "@/services/rate-limit"
 
 import { RateLimitConfig } from "@/domain/rate-limit"
 
+import { createRandomUserAndBtcWallet } from "test/helpers"
+
 afterEach(async () => {
   jest.restoreAllMocks()
 })
@@ -21,6 +23,7 @@ describe("addQuiz", () => {
     const result = await Quiz.claimQuiz({
       accountId: crypto.randomUUID() as AccountId,
       quizQuestionId: "fakeQuizQuestionId",
+      skipRewards: false,
       ip: undefined,
     })
     expect(result).toBeInstanceOf(InvalidIpMetadataError)
@@ -42,12 +45,51 @@ describe("addQuiz", () => {
     const result = await Quiz.claimQuiz({
       accountId: crypto.randomUUID() as AccountId,
       quizQuestionId: "fakeQuizQuestionId",
+      skipRewards: false,
       ip: "192.168.13.13" as IpAddress,
     })
 
     expect(result).toBeInstanceOf(AddQuizAttemptIpRateLimiterExceededError)
 
     // Restore system state
+    rateLimitServiceSpy.mockReset()
+  })
+
+  it("passes if ip is undefined and skipRewards is true", async () => {
+    const { accountId } = await createRandomUserAndBtcWallet()
+
+    const result = await Quiz.claimQuiz({
+      accountId,
+      quizQuestionId: "whoControlsBitcoin",
+      skipRewards: true,
+      ip: undefined,
+    })
+    expect(result).not.toBeInstanceOf(Error)
+  })
+
+  it("passes if ip limit hit but skipRewards is true", async () => {
+    const { RedisRateLimitService } = jest.requireActual("@/services/rate-limit")
+    const rateLimitServiceSpy = jest
+      .spyOn(RateLimitImpl, "RedisRateLimitService")
+      .mockReturnValue({
+        ...RedisRateLimitService({
+          keyPrefix: RateLimitConfig.addQuizAttemptPerIp.key,
+          limitOptions: RateLimitConfig.addQuizAttemptPerIp.limits,
+        }),
+        consume: () => new RateLimiterExceededError(),
+      })
+
+    const { accountId } = await createRandomUserAndBtcWallet()
+
+    const result = await Quiz.claimQuiz({
+      accountId,
+      quizQuestionId: "copyBitcoin",
+      skipRewards: true,
+      ip: "192.168.13.13" as IpAddress,
+    })
+
+    expect(result).not.toBeInstanceOf(Error)
+
     rateLimitServiceSpy.mockReset()
   })
 })
