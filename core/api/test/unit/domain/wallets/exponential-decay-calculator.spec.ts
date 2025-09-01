8fbb9c52cc4718473bef040188548d2525c8aae6
Esaú Gómez <alfonsoesaugomez@gmail.com>
2025-09-01 14:28:21 -0600

    fix(core): onchainfee using minerfee as bank cost in exponentialDecay method

--- core/api/test/unit/domain/wallets/exponential-decay-calculator.spec.ts
+++ core/api/test/unit/domain/wallets/exponential-decay-calculator.spec.ts
@@ -2,38 +2,25 @@ import { PayoutSpeed } from "@/domain/bitcoin/onchain"
 import { WalletCurrency } from "@/domain/shared"
 import { OnChainExpDecayFees } from "@/domain/wallets"
 
-import { generateTxSizeCases } from "test/helpers/withdrawal-fee-calculator"
 import {
-  costToBankCasesMock,
   feeCapCasesMock,
   multiplierCasesMock,
   onchainFeeSettingsMock,
-  TRANSACTION_SIZE_MATRIX,
 } from "test/mocks/exponential-decay-calculator"
 
-const txSizeCases = generateTxSizeCases(TRANSACTION_SIZE_MATRIX)
 const feeCalculator = OnChainExpDecayFees({
   onchain: onchainFeeSettingsMock,
 })
 
 describe("OnChainExpDecayFees", () => {
-  describe("calculateTransactionSize", () => {
-    test.each(txSizeCases)(
-      "inputs=%i, outputs=%i => %i bytes",
-      (inputs: number, outputs: number, expectedSize: number) => {
-        const size = feeCalculator.calculateTransactionSize(inputs, outputs)
-        expect(size).toBe(expectedSize)
-      },
-    )
-  })
-
   describe("expDecayFee", () => {
     describe("Tier #1 (Fast)", () => {
       test.each(feeCapCasesMock.tier1)(
         "Tier1: amount=$satsAmount sats, feeRate=$feeRate => bankFee $expectedSats sats",
-        ({ satsAmount, feeRate, expectedSats }) => {
+        ({ satsAmount, feeRate, expectedSats, minerFee }) => {
           const { totalFee } = feeCalculator.expDecayFee({
             amount: { amount: BigInt(satsAmount), currency: WalletCurrency.Btc },
+            minerFee: { amount: BigInt(minerFee), currency: WalletCurrency.Btc },
             speed: PayoutSpeed.Fast,
             feeRate,
           })
@@ -45,9 +32,10 @@ describe("OnChainExpDecayFees", () => {
     describe("Tier #2 (Medium)", () => {
       test.each(feeCapCasesMock.tier2)(
         "Tier2: amount=$satsAmount sats, feeRate=$feeRate => bankFee $expectedSats sats",
-        ({ satsAmount, feeRate, expectedSats }) => {
+        ({ satsAmount, feeRate, expectedSats, minerFee }) => {
           const { totalFee } = feeCalculator.expDecayFee({
             amount: { amount: BigInt(satsAmount), currency: WalletCurrency.Btc },
+            minerFee: { amount: BigInt(minerFee), currency: WalletCurrency.Btc },
             speed: PayoutSpeed.Medium,
             feeRate,
           })
@@ -59,44 +47,14 @@ describe("OnChainExpDecayFees", () => {
     describe("Tier #3 (Slow)", () => {
       test.each(feeCapCasesMock.tier3)(
         "Tier3: amount=$satsAmount sats, feeRate=$feeRate => bankFee $expectedSats sats",
-        ({ satsAmount, feeRate, expectedSats }) => {
+        ({ satsAmount, feeRate, expectedSats, minerFee }) => {
           const { totalFee } = feeCalculator.expDecayFee({
             amount: { amount: BigInt(satsAmount), currency: WalletCurrency.Btc },
+            minerFee: { amount: BigInt(minerFee), currency: WalletCurrency.Btc },
             speed: PayoutSpeed.Slow,
             feeRate,
           })
           expect(totalFee.amount).toEqual(BigInt(expectedSats))
-        },
-      )
-    })
-  })
-
-  describe("calculateCostToBank", () => {
-    describe("Tier #1 & #2 (Fast & Medium)", () => {
-      const cases = costToBankCasesMock.tier1_2
-
-      test.each(cases)(
-        "amount=$satsAmount sats, feeRate=$feeRate => costToBank $expectedCost sats",
-        ({ satsAmount, feeRate, expectedCost }) => {
-          expect(
-            feeCalculator.calculateCostToBank(satsAmount, PayoutSpeed.Fast, feeRate),
-          ).toBe(expectedCost)
-          expect(
-            feeCalculator.calculateCostToBank(satsAmount, PayoutSpeed.Medium, feeRate),
-          ).toBe(expectedCost)
-        },
-      )
-    })
-
-    describe("Tier #3 (Slow)", () => {
-      const cases = costToBankCasesMock.tier3
-
-      test.each(cases)(
-        "amount=$satsAmount sats, feeRate=$feeRate => costToBank $expectedCost sats",
-        ({ satsAmount, feeRate, expectedCost }) => {
-          expect(
-            feeCalculator.calculateCostToBank(satsAmount, PayoutSpeed.Slow, feeRate),
-          ).toBe(expectedCost)
         },
       )
     })
