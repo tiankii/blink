a183e53dde97b148aee6bd043baa19312b23af42
Esaú Gómez <alfonsoesaugomez@gmail.com>
2025-12-09 18:50:44 -0600

    feat(core|notifications): smg_templates and smg_messages tables

---
+++ core/notifications/migrations/20251209165958_create_msg_templates_and_msg_messages.up.sql
@@ -0,0 +1,28 @@
+-- Add up migration script here
+
+CREATE TABLE msg_templates (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name VARCHAR NOT NULL,
+  language_code VARCHAR NOT NULL,
+  icon_name VARCHAR NOT NULL,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  UNIQUE (name, language_code)
+);
+
+CREATE TABLE msg_messages (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  username VARCHAR NOT NULL,
+  status VARCHAR NOT NULL CHECK (status IN ('pending', 'accepted', 'revoked')),
+  sent_by VARCHAR NOT NULL,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_msg_templates_name_language_code
+  ON msg_templates (name, language_code);
+
+CREATE INDEX idx_msg_messages_username
+  ON msg_messages (username);
+
+CREATE INDEX idx_msg_messages_status
+  ON msg_messages (status);
