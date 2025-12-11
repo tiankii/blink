d3cd6e637521e96966e4150b9aeacdcfb4821dde
Juan Villatoro <juanvasquez02145@gmail.com>
2025-12-11 15:32:27 -0600

    feat: revoke invitation

--- apps/admin-panel/app/invitations/getMessages.tsx
+++ apps/admin-panel/app/invitations/getMessages.tsx
@@ -6,6 +6,10 @@ import {
   NotificationMessagesDocument,
   NotificationMessagesQuery,
   NotificationMessagesQueryVariables,
+  NotificationMessageUpdateStatusMutation,
+  NotificationMessageUpdateStatusMutationVariables,
+  NotificationMessageUpdateStatusDocument,
+  NotificationMessageUpdateStatusInput,
 } from "../../generated"
 
 export const getInvitations = async (): Promise<NotificationMessagesQuery> => {
@@ -16,6 +20,25 @@ export const getInvitations = async (): Promise<NotificationMessagesQuery> => {
     NotificationMessagesQueryVariables
   >({
     query: NotificationMessagesDocument,
+  })
+
+  return data
+}
+
+export const RevokeInvitation = async (data: NotificationMessageUpdateStatusInput) => {
+  console.warn("Revoke invitation")
+
+  await getClient().mutate<
+    NotificationMessageUpdateStatusMutation,
+    NotificationMessageUpdateStatusMutationVariables
+  >({
+    mutation: NotificationMessageUpdateStatusDocument,
+    variables: {
+      input: {
+        id: data.id,
+        status: data.status,
+      },
+    },
   })
 
   return data
